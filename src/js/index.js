f3730f37eaabf576cee2294cc2af0c4cb0a40958
Tomas Orrego <tom.orrego@gmail.com>
2021-05-19 00:51:32 -0400

    cambios filtro

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+//Modal para las imagenes de la galería
 $(document).ready(function () {
     $('.popupimage').click(function (event) {
         event.preventDefault();
@@ -6,6 +7,8 @@ $(document).ready(function () {
     });
 });
 
+
+//Función de jquery y validate que permite validar el contenido del formulario previamente a ser enviado
 $(document).ready(function () {
     $("#formulario").validate({
         rules: {
@@ -43,34 +46,13 @@ $(document).ready(function () {
     });
 });
 
-
-/* $(document).ready(() => {
-    document.querySelector("#buscar").addEventListener("click", (e) => {
-        e.preventDefault();
-        const busqueda = document.getElementById("#buscar-artista").value.trim();
-        if(busqueda) {
-            document.querySelectorAll("#autor").forEach(item => {
-                let autor = item.innerHTML;
-                if(busqueda == autor){
-                    console.log("Encontrado!!!");
-                    item.classList.remove("oculto");
-                }else{
-                    console.log("No se ha encontrado.");
-                }
-            });
-        }else {
-            console.log("escriba el nombre de un artista");
-        }
-    })
-}); */
-
-//Variables globales
+//Variables globales para la sección de api-dolar
 let divisaActual = 'USD';
-
 const galeria = document.querySelector("#mostrador");
 
 
-//Api convertir divisas
+//Api convertir divisas -- USD //Esta función consume la api para conseguir el valor del dolar y 
+//realiza las operaciones matemáticas para calcular el nuevo valor de cada elemento encontrado
 getValorDolar = (divisa, divisaActual) => {
     axios.get("https://mindicador.cl/api")
         .then(res => {
@@ -95,6 +77,7 @@ getValorDolar = (divisa, divisaActual) => {
         })
 }
 
+//llamamos a la funcion que consume la api mediante axios y que realiza el calculo del nuevo valor en cada elemento encontrado
 $(document).ready(function () {
     document.querySelectorAll(".divisaConv").forEach(item => {
         item.addEventListener("click", () => {
@@ -112,6 +95,7 @@ $(document).ready(function () {
     })
 });
 
+//cambiamos las siglas en el texto de la divisa seleccionada
 function cambiarDivImpresa(divisaActual) {
     document.querySelectorAll("#divisa").forEach((item) => {
         if (divisaActual == 'USD') {
@@ -124,6 +108,8 @@ function cambiarDivImpresa(divisaActual) {
 
 
 //Poblar la sección de galeria
+
+//constructor de objetos "obras"
 const obras = function (titulo, autor, imagen, precio, tecnica, medidas, annio) {
     this.titulo = titulo,
         this.autor = autor,
@@ -133,16 +119,17 @@ const obras = function (titulo, autor, imagen, precio, tecnica, medidas, annio) 
         this.medidas = medidas,
         this.annio = annio
 }
-
-const obra1 = new obras("Caballero y Luna", "Manuel de la Cuadra", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/1.png", "800", "Pintura al Óleo", "180cms x 100cms", "2018");
-const obra2 = new obras("Ciervo Poligonal", "Andrea Ramírez", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/2.png", "75", "Diseño Digital", "150cms x 95cms", "2020");
-const obra3 = new obras("Olas de Colores", "Romina García", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/3.png", "750", "Pintura al Óleo", "200cms x 150cms", "2006");
-const obra4 = new obras("Mujer del Bosque", "Gabriel Villarán", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/4.png", "1250", "Pintura al Óleo", "120cms x 75cms", "1998");
-const obra5 = new obras("Mujer del Bosque", "Gabriel Villarán", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/4.png", "1250", "Pintura al Óleo", "120cms x 75cms", "1998");
-const obra6 = new obras("Mujer del Bosque", "Gabriel Villarán", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/4.png", "1250", "Pintura al Óleo", "120cms x 75cms", "1998");
+// contenido simulación de información de backend para propositos de prueba
+const obra1 = new obras("Caballero y Luna", "Manuel de la Cuadra", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/1.png", "800", "Realismo", "180cms x 100cms", "2018");
+const obra2 = new obras("Ciervo Poligonal", "Andrea Ramírez", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/2.png", "75", "Impresionismo", "150cms x 95cms", "2020");
+const obra3 = new obras("Olas de Colores", "Romina García", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/3.png", "750", "Expresionismo", "200cms x 150cms", "2006");
+const obra4 = new obras("Mujer del Bosque", "Gabriel Villarán", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/4.png", "1250", "Surrealismo", "120cms x 75cms", "1998");
+const obra5 = new obras("Mujer del Bosque", "Gabriel Villarán", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/4.png", "1250", "Surrealismo", "120cms x 75cms", "1998");
+const obra6 = new obras("Mujer del Bosque", "Gabriel Villarán", "https://tomorregoa.github.io/ProyectoSemestral/src/images/gallery/4.png", "1250", "Surrealismo", "120cms x 75cms", "1998");
 
 const obrasArray = [obra1, obra2, obra3, obra4, obra5, obra6];
 
+// Recorremos el array con objetos "obra" y lo imprimimos en la sección de galeria
 function printObras() {
     obrasArray.forEach(function (item) {
         galeria.innerHTML += `
@@ -158,7 +145,7 @@ function printObras() {
                                 class="text-muted">/ <span id="divisa">USD</span></small></h1>
                         <ul class="list-unstyled mt-3 mb-4">
                             <li id="autor">${item.autor}</li>
-                            <li>Técnica: ${item.tecnica}</li>
+                            <li>Estilo: <span id="estilo">${item.tecnica}</span></li>
                             <li>Medidas: ${item.medidas}</li>
                             <li>Año: ${item.annio}</li>
                         </ul>
@@ -170,86 +157,59 @@ function printObras() {
 }
 
 
+
 $(document).ready(function () {
+    //Llamamos a la función de imprimir obras en galeria
     printObras();
-})
 
-
-$(document).ready(function () {
-    document.querySelector("#buscarArtista").addEventListener("click", (e)=>{
+    document.querySelector("#btnBuscador").addEventListener("click", (e) => {
         e.preventDefault();
-        let buscador = document.querySelector("#nombreArtista");
+
+        let filtroArtista = document.querySelector("#filtroArtista").value;
+        let filtroEstilo = document.querySelector("#filtroEstilo").value;
+        let mensaje = document.querySelector("#mensaje");
         let found = false;
-        if(buscador.value != ''){
-            document.querySelectorAll("#autor").forEach(item =>{
-                if(buscador.value == item.innerHTML){
+
+        mensaje.classList.add("oculto");
+
+        document.querySelectorAll("#carta").forEach(item => {
+            let autor = item.querySelector("#autor").innerHTML;
+            let estilo = item.querySelector("#estilo").innerHTML;
+
+            if (filtroArtista != '') {
+                if (filtroArtista == autor) {
+                    if (filtroEstilo == "Escoja Estilo" || filtroEstilo == estilo) {
+                        item.classList.remove("oculto");
+                        mensaje.classList.add("oculto");
+                        found = true;
+                    } else {
+                        item.classList.add("oculto");
+                        if (found == false) {
+                            mensaje.classList.remove("oculto");
+                        }
+                    }
+
+                } else {
+                    item.classList.add("oculto");
+                    if (found == false) {
+                        mensaje.classList.remove("oculto");
+                    }
+                }
+            } else if (filtroArtista == '' && filtroEstilo != "Escoja Estilo") {
+                if (filtroEstilo == estilo) {
+                    item.classList.remove("oculto");
+                    mensaje.classList.add("oculto");
                     found = true;
+                } else {
+                    item.classList.add("oculto");
+                    if (found == false) {
+                        mensaje.classList.remove("oculto");
+                    }
                 }
-                if(buscador.value != item.innerHTML || found != true){
-                    item.parentNode.parentNode.parentNode.classList.add("oculto");
-                    
-                }
-            });
-        }else{
-            document.querySelectorAll("#carta").forEach((item)=>{
+            } else if(filtroArtista == '' && filtroEstilo == "Escoja Estilo") {
                 item.classList.remove("oculto");
-            })
-        }
-        
-    })
-})
-
-
-//Filtro artistas galería
-/* document.querySelector("#buscarArtista").addEventListener("click", (e)=>{
-    let found = false;
-    e.preventDefault();
-    const nombreArtista = document.querySelector("#nombreArtista").value;
-    galeria.innerHTML = '';
-    if(nombreArtista != ''){
-        obrasArray.forEach(item =>{
-            let index = obrasArray.lastIndexOf(item);
-            let max = obrasArray.length - 1;
-            console.log(index);
-            console.log(max);
-            if(nombreArtista == item.autor){
-                found = true;
-                console.log("Obra encontrada: " + item.titulo);
-                galeria.innerHTML += `
-                <div class="card mb-4 box-shadow" id="obra">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">${item.titulo}</h4>
-                    </div>
-                    <div class="card-body gallery">
-                        <a class="popupimage" href="${item.imagen}">
-                            <img class="card-img-top" src="${item.imagen}">
-                        </a>
-                        <h1 class="precio">$<span id="valor">${item.precio}</span><small
-                                class="text-muted">/ <span id="divisa">USD</span></small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>${item.autor}</li>
-                            <li>Técnica: ${item.tecnica}</li>
-                            <li>Medidas: ${item.medidas}</li>
-                            <li>Año: ${item.annio}</li>
-                        </ul>
-                        <button type="button" class="btn btn-lg btn-block btn-danger">Ver Detalles</button>
-                    </div>
-                </div>`
-                getValorDolar = (divisaActual, divisaActual);
-            } else if(found == false && index==max) {
-                galeria.innerHTML = '';
-                galeria.innerHTML += '<p>Artista no Encontrado</p>';
+                mensaje.classList.add("oculto");
             }
         });
-
-    } else {
-        galeria.innerHTML = '';
-        printObras();
-    }
-});
-
-document.querySelector("#nombreArtista").addEventListener("search", ()=>{
-    galeria.innerHTML = '';
-    printObras();
+    })
 })
- */
